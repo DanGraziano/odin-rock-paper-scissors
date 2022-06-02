83d4ed32e37a42fee858592e28a01914588247c3
DanGraziano <dangraziano@outlook.com>
2022-06-01 22:02:56 -0400

    Add comments and fix formatting of if statement

--- script.js
+++ script.js
@@ -1,56 +1,66 @@
-function computerPlay() {
-    let selection = ["Rock", "Paper", "Scissors"];
-    let randomNumber = Math.floor(Math.random() * selection.length);
-    let randomSelection = selection[randomNumber];
-    return randomSelection;
+//Computer picks a Rock, Paper, or Scissors at random
 
+function computerPlay() {
+    let compPlay = ["Rock", "Paper", "Scissors"];
+    let randomNumber = Math.floor(Math.random() * compPlay.length);
+    let randomSelection = compPlay[randomNumber];
+    return randomSelection;
 }
+
+// User is asked to enter in Rock, Paper, or Scissors
 
 function playerInput (playerInput, input) {
     playerInput = prompt("Rock, Paper, or Scissors");
-    lowerCaseInput = playerInput.toLocaleLowerCase();
+    lowerCaseInput = playerInput.toLowerCase();
     return(lowerCaseInput);
-
 }
 
-const computerSelection = computerPlay();
+// Computer's move is stored in a constant. User's input is stored in a second constant
+
 const playerSelection = playerInput();
+const computerSelection = computerPlay();
+
+// Function for the first round with an if and else if statement based on user and comp selection
 
 function playRound (playerSelection, computerSelection) {
-    roundWinner = " ";
+    let roundWinner = " ";
     if (playerSelection === computerSelection) {
-        roundWinner = 'You both selected ${playerSelection}. It/s a tie'
+        roundWinner = `You both selected ${playerSelection}. It's a tie`
     }
     
-    else if (playerSelection == "rock" && computerSelection == "Scissors"){
-
+    else if (playerSelection == "rock" && computerSelection == "Scissors") {
         roundWinner = "You win! Rock beats Scissors!";
-    }
-
-    else if (playerSelection == "paper" && computerSelection == "Rock") {
-        roundWinner = "You win! Paper beats Rock!";
-    }
-
-    else if (playerSelection == "scissors" && computerSelection == "Paper") {
-        roundWinner = "You win! Scissors beats Paper!";
-    }
-
-    else if (playerSelection == "paper" && computerSelection == "Scissors") {
-        roundWinner = "You lose! Scissors beats Paper!";
     }
 
     else if (playerSelection == "rock" && computerSelection == "Paper") {
         roundWinner = "You lose! Paper beats Rock!";
     }
 
-    else if (playerSelection == "scissors" && computerSelection == "rock") {
+    else if (playerSelection == "paper" && computerSelection == "Rock") {
+        roundWinner = "You win! Paper beats Rock!";
+    }
+
+    else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        roundWinner = "You lose! Scissors beats Paper!";
+    }
+
+    else if (playerSelection == "scissors" && computerSelection == "Paper") {
+        roundWinner = "You win! Scissors beats Paper!";
+    }
+
+    else if (playerSelection == "scissors" && computerSelection == "Rock") {
         roundWinner = "You lose! Rock beats Scissors!";
     }
 
-    else (roundWinner = "Something went wrong. Try again") 
+    else {
+        roundWinner = "Something went wrong. Please try again";
+    }
     
     return roundWinner;
 
 }
 
+console.log(computerPlay())
+console.log(computerSelection)
+console.log(lowerCaseInput)
 console.log(playRound(playerSelection, computerSelection));
