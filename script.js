9fb93a5df49f236c52b8546e4470efb92a9274a6
DanGraziano <dangraziano@outlook.com>
2022-06-01 21:26:16 -0400

    Add playRound function and if statement

--- script.js
+++ script.js
@@ -11,14 +11,49 @@ function computerPlay() {
 
 }
 
-function playerInput (playerInput, input) {
+function playerSelection (playerInput, input) {
     playerInput = prompt("Rock, Paper, or Scissors");
-    lowerCaseInput = playerInput.toLocaleLowerCase();
-    return(lowerCaseInput);
+    input = playerInput.toLocaleLowerCase();
+    return(input);
 
 }
 
 const computerSelection = computerPlay();
-const playerSelection = playerInput();
 
+function playRound (playerSelection, computerSelection) {
+    roundWinner = " ";
+    if (playerSelection === computerSelection) {
+        roundWinner = 'You both selected ${playerSelection}. It/s a tie'
+    }
+    
+    else if (playerSelection == "rock" && computerSelection == "Scissors"){
 
+        roundWinner = "You win! Rock beats Scissors!";
+    }
+
+    else if (playerSelection == "paper" && computerSelection == "Rock") {
+        roundWinner = "You win! Paper beats Rock!";
+    }
+
+    else if (playerSelection == "scissors" && computerSelection == "Paper") {
+        roundWinner = "You win! Scissors beats Paper!";
+    }
+
+    else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        roundWinner = "You lose! Scissors beats Paper!";
+    }
+
+    else if (playerSelection == "rock" && computerSelection == "Paper") {
+        roundWinner = "You lose! Paper beats Rock!";
+    }
+
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        roundWinner = "You lose! Rock beats Scissors!";
+    }
+
+    else (roundWinner = "Something went wrong. Try again") 
+    return roundWinner;
+
+}
+
+console.log(playRound(playerSelection(), computerSelection));
