d010f92c7a4f0466cabc7c653d4b359acc67936a
DanGraziano <dangraziano@outlook.com>
2022-06-02 21:58:12 -0400

    Remove variable from playRound and replace with return

--- script.js
+++ script.js
@@ -26,40 +26,37 @@ const computerSelection = computerPlay();
 // Function for the first round with an if and else if statement based on user and comp selection
 
 function playRound (playerSelection, computerSelection) {
-    let roundWinner = " ";
     if (playerSelection === computerSelection) {
-        roundWinner = `You both selected ${playerSelection}. It's a tie`
+        return `You both selected ${playerSelection}. It's a tie`
     }
     
     else if (playerSelection == "rock" && computerSelection == "scissors") {
-        roundWinner = "You win! Rock beats Scissors!";
+        return "You win! Rock beats Scissors!";
     }
 
     else if (playerSelection == "rock" && computerSelection == "paper") {
-        roundWinner = "You lose! Paper beats Rock!";
+        return "You lose! Paper beats Rock!";
     }
 
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        roundWinner = "You win! Paper beats Rock!";
+        return "You win! Paper beats Rock!";
     }
 
     else if (playerSelection == "paper" && computerSelection == "scissors") {
-        roundWinner = "You lose! Scissors beats Paper!";
+        return "You lose! Scissors beats Paper!";
     }
 
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        roundWinner = "You win! Scissors beats Paper!";
+        return "You win! Scissors beats Paper!";
     }
 
     else if (playerSelection == "scissors" && computerSelection == "rock") {
-        roundWinner = "You lose! Rock beats Scissors!";
+        return "You lose! Rock beats Scissors!";
     }
 
     else {
-        roundWinner = "Something went wrong. Please try again";
+        return "Something went wrong. Please try again";
     }
-    
-    return roundWinner;
 
 }
 
